7a54aeac470ee0cc072f4a39ca436d62b46f630e
Lars Fenneberg <lf@elemental.net>
2017-12-06 21:18:37 +0100

    Split up networking resources into shared and non-shared directories. Shared means that they are shared with the canal networking setup.
    First I tried to use symbolic links to share files between canal and the
    rest but this didn't work out:
    
    Terraform 0.11.0 doesn't support symbolic links in the source directory of
    the template_dir resource.  This is because changes in this directory are
    detected by building a tar archive on the fly and calculating a hash over
    it.  The tar archive building doesn't support symlinks currently.  This
    could be fixed but would require changes in Terraform obviously.

--- conditional.tf
+++ conditional.tf
@@ -1,5 +1,18 @@
 # Assets generated only when certain options are chosen
 
+resource "template_dir" "flannel-manifests-shared" {
+  count           = "${var.networking == "flannel" || var.networking == "canal" ? 1 : 0}"
+  source_dir      = "${path.module}/resources/flannel-shared"
+  destination_dir = "${var.asset_dir}/manifests-networking"
+
+  vars {
+    flannel_image     = "${var.container_images["flannel"]}"
+    flannel_cni_image = "${var.container_images["flannel_cni"]}"
+
+    pod_cidr = "${var.pod_cidr}"
+  }
+}
+
 resource "template_dir" "flannel-manifests" {
   count           = "${var.networking == "flannel" ? 1 : 0}"
   source_dir      = "${path.module}/resources/flannel"
@@ -10,6 +23,20 @@ resource "template_dir" "flannel-manifests" {
     flannel_cni_image = "${var.container_images["flannel_cni"]}"
 
     pod_cidr = "${var.pod_cidr}"
+  }
+}
+
+resource "template_dir" "calico-manifests-shared" {
+  count           = "${var.networking == "calico" || var.networking == "canal" ? 1 : 0}"
+  source_dir      = "${path.module}/resources/calico-shared"
+  destination_dir = "${var.asset_dir}/manifests-networking"
+
+  vars {
+    calico_image     = "${var.container_images["calico"]}"
+    calico_cni_image = "${var.container_images["calico_cni"]}"
+
+    network_mtu = "${var.network_mtu}"
+    pod_cidr    = "${var.pod_cidr}"
   }
 }
 
