1979cbbdea3421c1eaade73a2cd2481ac99da1bc
Dalton Hubble <dghubble@gmail.com>
2019-09-29 11:14:20 -0700

    Update Calico from v3.8.2 to v3.9.1
    * https://docs.projectcalico.org/v3.9/release-notes/
    
    (cherry picked from commit 18b7a74d309da7290474e35701bc6668f0dd035e)

--- variables.tf
+++ variables.tf
@@ -76,9 +76,9 @@ variable "container_images" {
   type = map(string)
 
   default = {
-    calico = "quay.io/calico/node:v3.8.0"
-    calico_cni = "quay.io/calico/cni:v3.8.0"
-    flannel = "quay.io/coreos/flannel:v0.11.0-amd64"
+    calico      = "quay.io/calico/node:v3.9.1"
+    calico_cni  = "quay.io/calico/cni:v3.9.1"
+    flannel     = "quay.io/coreos/flannel:v0.11.0-amd64"
     flannel_cni = "quay.io/coreos/flannel-cni:v0.3.0"
     kube_router = "cloudnativelabs/kube-router:v0.3.1"
     hyperkube = "k8s.gcr.io/hyperkube:v1.15.1"
