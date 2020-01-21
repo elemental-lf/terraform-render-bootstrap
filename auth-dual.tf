4ea661504b56421368de3bf02a4e5c53af18cfbd
Lars Fenneberg <lf@elemental.net>
2020-01-21 13:39:16 +0100

    Add missing files

---
+++ auth-dual.tf
@@ -0,0 +1,28 @@
+#
+# This renders a special kubeconfig to access the local apiserver directly.
+# This is used for health checking purposes.
+#
+locals {
+  auth_kubeconfigs_dual = {
+    "auth/kubeconfig-localhost" = data.template_file.kubeconfig-admin-localhost.rendered,
+  }
+}
+
+data "template_file" "kubeconfig-admin-localhost" {
+  template = file("${path.module}/resources/kubeconfig-admin")
+
+  vars = {
+    name         = var.cluster_name
+    ca_cert      = base64encode(tls_self_signed_cert.kube-ca.cert_pem)
+    kubelet_cert = base64encode(tls_locally_signed_cert.admin.cert_pem)
+    kubelet_key  = base64encode(tls_private_key.admin.private_key_pem)
+    server       = format("https://localhost:%s", var.external_apiserver_port)
+  }
+}
+
+resource "local_file" "kubeconfig-admin-localhost" {
+  count = var.asset_dir == "" ? 0 : 1
+
+  content  = data.template_file.kubeconfig-admin-localhost.rendered
+  filename = "${var.asset_dir}/auth/kubeconfig-localhost"
+}
---
+++ manifests-dual.tf
@@ -0,0 +1,41 @@
+locals {
+  helm_manifests = {
+  for name in fileset("${path.module}/resources/helm", "**/*.yaml") :
+  "manifests/${name}" => templatefile(
+  "${path.module}/resources/helm/${name}",
+  {
+    tiller_image = var.container_images["tiller"]
+
+    helm_ca_cert = base64encode(tls_self_signed_cert.helm_ca.cert_pem)
+    tiller_cert  = base64encode(tls_locally_signed_cert.tiller.cert_pem)
+    tiller_key   = base64encode(tls_private_key.tiller.private_key_pem)
+  }
+  )
+  }
+
+  extra_assert_apiserver_vip = {
+  for name in fileset("${path.module}/resources/apiserver-vip", "**/*.{conf,sh}") :
+  "extra-assets/apiserver-vip/${name}" => templatefile(
+  "${path.module}/resources/apiserver-vip/${name}",
+  {
+    apiserver_vip = var.apiserver_vip
+    apiserver_vip_interface = "bond0"
+    apiserver_vip_vrrp_id = 178
+  }
+  )
+  }
+}
+
+resource "local_file" "helm-manifests" {
+  for_each = var.asset_dir == "" ? {} : local.helm_manifests
+
+  content  = each.value
+  filename = "${var.asset_dir}/${each.key}"
+}
+
+resource "local_file" "extra-assets-apiserver-vip" {
+  for_each = var.asset_dir == "" ? {} : local.extra_assert_apiserver_vip
+
+  content  = each.value
+  filename = "${var.asset_dir}/${each.key}"
+}
