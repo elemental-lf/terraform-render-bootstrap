2e2c4d6010ee500785ec85d90106367ab263b59b
Lars Fenneberg <lf@elemental.net>
2020-04-27 19:46:00 +0200

    Update Kubernetes to 1.18.2 and Calico to 3.13.3

--- variables.tf
+++ variables.tf
@@ -70,16 +70,16 @@ variable "container_images" {
   description = "Container images to use"
 
   default = {
-    calico                  = "quay.io/calico/node:v3.13.1"
-    calico_cni              = "quay.io/calico/cni:v3.13.1"
+    calico                  = "quay.io/calico/node:v3.13.3"
+    calico_cni              = "quay.io/calico/cni:v3.13.3"
     coredns                 = "k8s.gcr.io/coredns:1.6.7"
-    flannel                 = "quay.io/coreos/flannel:v0.12.0-amd64"
+    flannel                 = "quay.io/coreos/flannel:v0.13.0-amd64"
     flannel_cni             = "quay.io/coreos/flannel-cni:v0.3.0"
-    kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.18.1"
-    kube_controller_manager = "k8s.gcr.io/kube-controller-manager:v1.18.1"
-    kube_scheduler          = "k8s.gcr.io/kube-scheduler:v1.18.1"
-    kube_proxy              = "k8s.gcr.io/kube-proxy:v1.18.1"
-    kubelet                 = "quay.io/poseidon/kubelet:v1.18.1"
+    kube_apiserver          = "k8s.gcr.io/kube-apiserver:v1.18.2"
+    kube_controller_manager = "k8s.gcr.io/kube-controller-manager:v1.18.2"
+    kube_scheduler          = "k8s.gcr.io/kube-scheduler:v1.18.2"
+    kube_proxy              = "k8s.gcr.io/kube-proxy:v1.18.2"
+    kubelet                 = "quay.io/poseidon/kubelet:v1.18.2"
     # experimental
     kube_router = "cloudnativelabs/kube-router:v0.3.2"
     #
